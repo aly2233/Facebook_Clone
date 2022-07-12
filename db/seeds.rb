0adf8d17708170a4494a52eaf86712f4c7d16c7b
Alan <alanleeyueh@gmail.com>
2022-07-12 13:47:06 -0400

    add back seed comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 require 'open-uri'
 
-# User.destroy_all
-# Post.destroy_all
-# Comment.destroy_all
-# Like.destroy_all
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+Like.destroy_all
 
 demoUser = User.create!(email: 'demo@demo.com', password: 'demodemo', first_name: 'Demo', last_name: 'Demo', birthday: '1999-01-01', bio: "I am a demo", hometown: "Demo, Demo", current_town: "Demo, Demo", relationship: "Single", school: "Demo University")
 demoUser.cover_picture.attach(io: File.open("app/assets/images/default_cover_picture.png"), filename: "default_cover_picture.png")
